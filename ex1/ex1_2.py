0b64fa8310aa1cfd2a49ed057d06a54d448c9794
Nadav Weisler <32937058+nadavWeisler@users.noreply.github.com>
2021-05-05 20:12:34 +0300

    Continue

--- ex1/ex1_2.py
+++ ex1/ex1_2.py
@@ -8,55 +8,27 @@ class Node:
         self.right = right
 
 
-def tree_size(node):
-    if node is None:
-        return 0
-    else:
-        return (tree_size(node.left) + 1 + tree_size(node.right))
+def lowestCommonAncestor(root, p, q):
+    if not root:
+        return None
+    if root.word == p or root.word == q:
+        return root
+    left = lowestCommonAncestor(root.left, p, q)
+    right = lowestCommonAncestor(root.right, p, q)
+    if right and left:
+        return root
+    return right or left
+    return left_subtree
 
 
-def findPath(root, path, k):
-    # Baes Case
-    if root is None:
-        return False
-
-    # Store this node is path vector. The node will be
-    # removed if not in path from root to k
-    path.append(root.key)
-
-    # See if the k is same as root's key
-    if root.key == k:
-        return True
-
-    # Check if k is found in left or right sub-tree
-    if ((root.left != None and findPath(root.left, path, k)) or
-            (root.right != None and findPath(root.right, path, k))):
-        return True
-
-    # If not present in subtree rooted with root, remove
-    # root from path and return False
-
-    path.pop()
-    return False
-
-
-def findLCA(root, n1, n2):
-    # To store paths to n1 and n2 fromthe root
-    path1 = []
-    path2 = []
-
-    # Find paths from root to n1 and root to n2.
-    # If either n1 or n2 is not present , return -1
-    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
-        return -1
-
-    # Compare the paths to get the first different value
-    i = 0
-    while (i < len(path1) and i < len(path2)):
-        if path1[i] != path2[i]:
-            break
-        i += 1
-    return path1[i - 1]
+def get_word_frequency(word):
+    file = open("./corpus_ex1.freq_list", "r")
+    invalid_word = 1
+    for line in file:
+        line_data = line.split()
+        if line_data[0] == word.lower():
+            return int(line_data[1])
+    return invalid_word
 
 
 def create_tree():
@@ -87,16 +59,64 @@ def create_tree():
     return rock
 
 
-def get_p_c(current, root):
-    return tree_size(current) / tree_size(root)
+def Print_Tree(root):
+    if root is None:
+        return
+    Print_Tree(root.left)
+    print(root.data)
+    Print_Tree(root.right)
 
 
-def sim_lin(node1, node2, root):
-    return (
-            2 * get_p_c(findLCA(root, node1, node2), root) /
-            (math.log(get_p_c(node1, root)) + math.log(get_p_c(node2, root)))
-            )
+def lin_on_node(node, node2, all_tree_size, root):
+    if node2 is None:
+        return
+    lin_on_node(node, node2.left, all_tree_size, root)
+    print("Lin on: " + str(node.word) + " & " + str(node2.word))
+    print(sim_lin(node, node2, all_tree_size, root))
+    print()
+    lin_on_node(node, node2.right, all_tree_size, root)
+
+
+def lin_on_tree(node, root, all_tree_size):
+    if node is None:
+        return
+    lin_on_tree(node.left, root, all_tree_size)
+    lin_on_node(node, root, all_tree_size, root)
+    lin_on_tree(node.right, root, all_tree_size)
+
+
+def get_p_c(current, all_tree_freq):
+    return get_all_tree_frequency(current) / all_tree_freq
+
+
+def sim_lin(node1, node2, all_tree_freq, root):
+    lca = lowestCommonAncestor(root, node1.word, node2.word)
+    up = 2 * math.log(get_p_c(lca, all_tree_freq))
+    down = math.log(get_p_c(node1, all_tree_freq)) + \
+        math.log(get_p_c(node2, all_tree_freq))
+    if down == 0:
+        return 0
+    return up / down
+
+
+def get_all_tree_frequency(root):
+    if root is None:
+        return 0
+    return get_word_frequency(root.word) + get_all_tree_frequency(root.right) + get_all_tree_frequency(root.left)
 
 
 if __name__ == '__main__':
     rock_root = create_tree()
+    all_tree_freq = get_all_tree_frequency(rock_root)
+    lin_on_tree(rock_root, rock_root, all_tree_freq)
+
+# We took the tree written on create_tree function
+# We applied lin similarity on every 2 nodes in the tree
+# We will discus on 5 combinations:
+
+# makes more sense:
+# treasure & gem more similar than treasure & stone -> Gem is consider treasure but stone is not
+#  controversy & disputation more similar than controversy & debate -> controversy & disputation are similar words, debate has different meaning.
+# hell & pit more similar than hell & stone -> Known prase is "pit of hell" and hell considered to be underground, stone is not something hell relative.
+# stone & pit more similar pit & muffin -> Pit can be fills with stone, but not with muffins (and its a shame)
+# muffin & gem more similar than muffin & hell -> Muffin is a gem, not hell.
